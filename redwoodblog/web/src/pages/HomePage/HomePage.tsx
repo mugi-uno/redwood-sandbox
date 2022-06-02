bed8e9e5e098395bc55cd06ee987548ea1777755
mugi-uno <mugi.uno@gmail.com>
2022-06-02 22:35:31 +0900

    completed Chapter 2

--- redwoodblog/web/src/pages/HomePage/HomePage.tsx
+++ redwoodblog/web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import { MetaTags } from '@redwoodjs/web'
+import ArticleCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      Home
+      <ArticleCell />
     </>
   )
 }
